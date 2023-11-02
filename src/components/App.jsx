42628e5145a6f346c2b48bcc0cd089aae9b214b9
dariameln1 <dariamelnykova1@gmail.com>
2023-11-02 23:03:12 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import { Profile } from './Profile/Profile';
 import user from '../user.json';
-import { Statistics } from './Statistics/Statistics';
-import data from '../data.json';
+// import { Statistics } from './Statistics/Statistics';
+// import data from '../data.json';
 import { FriendList } from './FriendList/FriendList';
 import friends from '../friends.json';
-import { TransactionHistory } from './TransactionHistory/TransactionHistory';
-import transactions from '../transactions.json';
+// import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+// import transactions from '../transactions.json';
 
 export const App = () => {
   return (
