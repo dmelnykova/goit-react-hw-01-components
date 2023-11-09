0d536d1291109f94e0eaf696a62aef8d1b39c2e2
dariameln1 <dariamelnykova1@gmail.com>
2023-11-09 19:23:38 +0100

    profile css

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,35 +1,40 @@
 import {
-  UserCard,
-  Avatar,
-  UserName,
-  StatsList,
-  StatsItem,
+    ProfileContainer,
+    UserCard,
+    Avatar,
+    UserName,
+    UserText,
+    StatsList,
+    StatsItem,
+    StatsSpan,
 } from './Profile.styled';
 
 export const Profile = ({
     user: { username, tag, location, avatar, stats }
 }) => {
   return (
-    <UserCard>
-        <Avatar src={avatar} alt={username} width="80" height="80"/>
-        <UserName>{username}</UserName>
-        <p>@{tag}</p>
-        <p>{location}</p>
-        <StatsList>
-            <StatsItem>
-                Followers
-                <span>{stats.followers}</span>
-            </StatsItem>
-            <StatsItem>
-                Views
-                <span>{stats.views}</span>    
-            </StatsItem>
-            <StatsItem>
-                Likes
-                <span>{stats.likes}</span>    
-            </StatsItem>
-        </StatsList>
-    </UserCard>
+    <ProfileContainer>
+        <UserCard>
+            <Avatar src={avatar} alt={username} width="110" height="110"/>
+            <UserName>{username}</UserName>
+            <UserText>@{tag}</UserText>
+            <UserText>{location}</UserText>
+            <StatsList>
+                <StatsItem>
+                    Followers
+                    <StatsSpan>{stats.followers}</StatsSpan>
+                </StatsItem>
+                <StatsItem>
+                    Views
+                    <StatsSpan>{stats.views}</StatsSpan>    
+                </StatsItem>
+                <StatsItem>
+                    Likes
+                    <StatsSpan>{stats.likes}</StatsSpan>    
+                </StatsItem>
+            </StatsList>
+        </UserCard>
+    </ProfileContainer>
   );
 };
 
--- src/components/Profile/Profile.styled.js
+++ src/components/Profile/Profile.styled.js
@@ -1,37 +1,47 @@
 import styled from 'styled-components';
 
+export const ProfileContainer = styled.div`
+    background-color: #F0FFF0;
+    max-width: 750px;
+    margin: auto;
+    padding-top: 50px;
+    padding-bottom: 50px;
+`;
+
 export const UserCard = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    width: 300px;
-    height: 370px;
+    background-color: #F5FFFA;
+    width: 410px;
+    height: 418px;
     margin: 10px auto 0 auto;
-    border: 1px solid black;
-    border-radius: 5px;
+    box-shadow: 0 10px 10px rgb(95, 158, 160);
 `;
 
 export const Avatar = styled.img`
-    width: 100px;
-    height: 100px;
     border-radius: 100%;
-    margin-top: 15px;
+    margin-top: 30px;
 `;
-
-
-
 
 export const UserName = styled.b`
-    margin-top: 30px;
+    margin-top: 25px;
     font-size: 24px;
-    font-weight: 900;
 `;
+
+export const UserText = styled.p`
+    margin-top: 10px;
+    margin-bottom: 15px;
+    color: #808080;
+    font-size: 18px;
+`;
+
+
 export const StatsList = styled.ul`
     display: flex;
     width: 100%;
-    justify-content: center;
-    list-style: none;
     padding: 0;
+    margin-top: 54px;
 
 `;
 export const StatsItem = styled.li`
@@ -40,11 +50,12 @@ export const StatsItem = styled.li`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    background-color: #13e0d8;
-    border-radius: 5px;
+    background-color: #87CEFA;
     height: 78px;
-    border: 1px solid grey;
+    border: 1px solid #E0FFFF;
+    font-size: 20px;
+`;
 
-
-
+export const StatsSpan = styled.span`
+    font-weight: bold;
 `;
